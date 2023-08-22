1fd52cf30b5edac55877ad2ed1ecdd1d3cb90252
lazlanrafar <lazlanrafar@gmail.com>
2023-08-23 01:25:33 +0700

    refactor login

--- login.php
+++ login.php
@@ -1,89 +1,107 @@
 <?php
 session_start();
 
-if(isset($_SESSION["login"])) {
-	header("Location: index.php");
-	exit;
+if (isset($_SESSION['login'])) {
+    header('Location: index.php');
+    exit();
 }
 
 require 'function.php';
 
-if (isset($_POST["login"])) {
+if (isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-	$username = $_POST["username"];
-	$password = $_POST["password"];
+    $result = mysqli_query($conn, "SELECT * FROM user WHERE username = '$username'");
 
-	$result = mysqli_query($conn, "SELECT * FROM user WHERE username = '$username'");
+    // cek username
+    if (mysqli_num_rows($result) === 1) {
+        // cek password
+        $row = mysqli_fetch_assoc($result);
+        if (password_verify($password, $row['password'])) {
+            // set session
+            $_SESSION['login'] = true;
+            $_SESSION['user_akses'] = $row['user_akses'];
 
-	// cek username
-	if (mysqli_num_rows($result) === 1 ) {
-		
-		// cek password 
-		$row = mysqli_fetch_assoc($result);
-		if ( password_verify($password, $row["password"])) {
-			// set session
-			$_SESSION["login"] = true;
-			$_SESSION["user"] = $row;
-			$_SESSION["user_akses"] = $row["user_akses"];
+            if ($row['user_akses'] == 'Super Admin') {
+                header('Location: index.php');
+            } elseif ($row['user_akses'] == 'Marketing') {
+                header('Location: marketing.php');
+            } elseif ($row['user_akses'] == 'Admin Operasional') {
+                header('Location: admin_operasional.php');
+            } elseif ($row['user_akses'] == 'Operasional Training') {
+                header('Location: operasional_training.php');
+            }
 
-			if ($row["user_akses"] == 'Super Admin') {
-				header("Location: index.php");
-			} else if ($row["user_akses"] == 'Marketing'){
-				header("Location: marketing.php");
-
-			}else if ($row["user_akses"] == 'Admin Operasional'){
-				header("Location: admin_operasional.php");
-
-			}else if ($row["user_akses"] == 'Operasional Training'){
-				header("Location: operasional_training.php");
-			}
-
-			exit;	
-		}
-	} else {
-			echo "
-			<script>
-			document.location.href = '';
-			</script>
-		";
-	}
-	$error = true;
+            exit();
+        } else {
+            $error = 'Password salah!';
+        }
+    } else {
+        $error = 'Username tidak ditemukan!';
+    }
 }
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	
-	<title></title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-</head>
+<?php include 'partials/head.php'; ?>
+
 <body>
-<div class="main">
-  <?php if (isset($error)) :  ?>
-	<?php echo "<script> alert ('Username / Password Salah')</script>"?>
-	<?php endif; ?>
+    <main class="d-flex w-100">
+        <div class="container d-flex flex-column">
+            <div class="row vh-100">
+                <div class="col-sm-10 col-md-8 col-lg-6 col-xl-5 mx-auto d-table h-100">
+                    <div class="d-table-cell align-middle">
 
-   	<div class ="login">
-   	
-   	<form action="" method="post">
-			<h4 class="text-center">LOGIN</h4>
-			<label for="username">Username</label>
-			<input type="text" name="username" id="username" placeholder="Masukkan Username Anda!" autofocus autocomplete="off">	
-	
-			<label for="password">Password</label>
-			<input type="password" name="password" id="password" placeholder="Masukkan Password Anda!" >	
-	
-			<button type="submit" name="login" class="btn btn-primary">Login</button>
-	
-	</form>
-</div>
-</div>
-
+                        <div class="text-center mt-4">
+                            <h1 class="h2">Welcome back!</h1>
+                            <p class="lead">
+                                Sign in to your account to continue
+                            </p>
+                        </div>
+                        <?php if (isset($error)) :  ?>
+                        <div class="alert alert-danger" role="alert">
+                            <?php echo $error; ?>
+                        </div>
+                        <?php endif; ?>
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="m-sm-3">
+                                    <form action="" method="POST">
+                                        <div class="mb-3">
+                                            <label class="form-label">Username</label>
+                                            <input class="form-control form-control-lg" type="text" name="username"
+                                                placeholder="Enter your username" />
+                                        </div>
+                                        <div class="mb-3">
+                                            <label class="form-label">Password</label>
+                                            <input class="form-control form-control-lg" type="password" name="password"
+                                                placeholder="Enter your password" />
+                                        </div>
+                                        <div>
+                                            <div class="form-check align-items-center">
+                                                <input id="customControlInline" type="checkbox" class="form-check-input"
+                                                    value="remember-me" name="remember-me" checked>
+                                                <label class="form-check-label text-small"
+                                                    for="customControlInline">Remember me</label>
+                                            </div>
+                                        </div>
+                                        <div class="d-grid gap-2 mt-3">
+                                            <button type="submit" name="login" class="btn btn-lg btn-primary">
+                                                Sign in
+                                            </button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <?php include 'partials/script.php'; ?>
 </body>
+
 </html>
-
-
-
