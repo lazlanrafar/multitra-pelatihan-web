5ec04a944bed0d219fc89c051a9bcf6a8b23dec2
lazlanrafar <lazlanrafar@gmail.com>
2023-08-29 23:56:15 +0700

    delete data table

--- partials/head.php
+++ partials/head.php
@@ -8,7 +8,6 @@
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css"
         integrity="sha384-b6lVK+yci+bfDmaY1u0zE8YYJt0TZxLEAFyYSLHId4xoVvsrQu3INevFKo+Xir8e" crossorigin="anonymous" />
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.css" />
 
     <link href="assets/css/app.css" rel="stylesheet" />
 
